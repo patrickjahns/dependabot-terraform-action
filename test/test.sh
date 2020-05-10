80c3b36348b781e67aa07b00f8358982df272e50
Patrick Jahns <patrick.jahns@emnify.com>
2020-05-10 15:12:31 +0200

    feat: allow for specifying multiple paths in the repository
    Signed-off-by: Patrick Jahns <patrick.jahns@emnify.com>

--- test/test.sh
+++ test/test.sh
@@ -55,10 +55,30 @@ test_container_runs_successfully_on_local_repository() {
     print_nl
 }
 
+test_container_runs_successfully_on_local_repository_with_multiple_dirs() {
+    print_start "Testing the container runs successfully"
+    docker run -e INPUT_TARGET_BRANCH="master" \
+               -e INPUT_GITHUB_DEPENDENCY_TOKEN="${DEPENDENCY_GITHUB_TOKEN}" \
+               -e INPUT_TOKEN="${GITHUB_TOKEN}" \
+               -e GITHUB_REPOSITORY="patrickjahns/dependabot-terraform-action" \
+               -e INPUT_DIRECTORY="/test/terraform\n/test/second" \
+               --rm ${CONTAINER_IMAGE}:${CONTAINER_SHA}
+    retVal=$?
+    print_end
+    if [[ ${retVal} -ne 0 ]]; then
+      print_error "FAILURE"
+      RESULT=1
+    else
+      print_success "SUCCESS"
+    fi
+    print_nl
+}
+
 main() {
     print_default "Starting test suite .."
     pushd $SCRIPT_DIR > /dev/null
         test_container_runs_successfully_on_local_repository
+        test_container_runs_successfully_on_local_repository_with_multiple_dirs
     popd > /dev/null
     if [[ ${RESULT} -ne 0 ]]; then
       print_error "FAILURE"
